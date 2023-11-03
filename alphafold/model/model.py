f503a7bb40c263b51e303fa357b9354c066aaf66
sarah <sarahnd97@gmail.com>
2023-11-03 14:00:49 +0100

    log feat.keys() instead of feat.shape

--- alphafold/model/model.py
+++ alphafold/model/model.py
@@ -179,7 +179,7 @@ class RunModel:
     self.init_params(feat)
     logging.info('Running predict with shape(feat) = %s', tree.map_structure(lambda x: x.shape, feat))
     logging.info("################### Self.apply start #################################")
-    logging.info("feat: %s", str(feat.shape))
+    logging.info("feat: %s", str(feat.keys()))
     #logging.info("self.params: %s", str(self.params))
     result = self.apply(self.params, jax.random.PRNGKey(random_seed), feat) # this is where it goes wrong
     logging.info("################### Self.apply finished #################################")

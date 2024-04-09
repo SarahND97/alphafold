2a4873f16ccf0c999f55df9fabf587ba569a0bab
sarah <sarahnd97@gmail.com>
2024-04-09 17:01:43 +0200

    change dirs and singularity statement

--- run_alphafold.sh
+++ run_alphafold.sh
@@ -1,20 +1,25 @@
-documents=/home/sarahnd/Documents
-singularity_img=$documents/evoformer_alphafold/singularity-image/alphafold_v2.3.1.sif
-disk=/media/sarahnd/f4aa44bb-cc6e-486a-bc4c-7b4f1193d8b4/msas_2018_benchmark
+home=/home/sarahnd
+documents=$home/Documents
+evoformer=$documents/evoformer_alphafold/
+singularity_img=$evoformer/singularity-image/alphafold_v2.3.1.sif
+disk=$home/mnt/sarahnd/msas_2018_benchmark
 # Choose one of
 # heteromers  homomers  negative_heteromers  neg_homodimers
 type=heteromers
 msas=$disk/$type
 pdbid=5oxz
 msas_specific=$msas/$pdbid/msas
+FASTA_PATH=$documents/D-SCRIPT/data/seqres/$type
+alpha_dir=$evoformer/alphafold
+PARAMS=$alpha_dir/afdb
 output=$disk/pkl-files
-FASTA_PATH=$documents/D-SCRIPT/data/$type
 
-$singularity_img python3 run_alphafold.py \
-    --fasta_paths=$FASTA_PATH/$pdbid.fasta \
-    --model_preset=multimer \
-    --msa_dir=$msas_specific \
-    --output_dir=$output \
-    --data_dir=$PARAM \
-    --use_gpu_relax=False 
+singularity exec --nv --bind $home:$home $singularity_img \
+    python3 $alpha_dir/run_alphafold.py \
+        --fasta_paths=$FASTA_PATH/$pdbid.fasta \
+        --model_preset=multimer \
+        --msa_dir=$msas_specific \
+        --output_dir=$output \
+        --data_dir=$PARAMS \
+        --use_gpu_relax=False \
 

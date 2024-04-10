baaca1842effffb8bb8bbe72af4ee96ee4c57cb2
sarah <sarahnd97@gmail.com>
2024-04-10 11:52:49 +0200

    make it possible to loop through lines in a file and make use of new functionality

--- run_alphafold.sh
+++ run_alphafold.sh
@@ -7,19 +7,28 @@ disk=$home/mnt/sarahnd/msas_2018_benchmark
 # heteromers  homomers  negative_heteromers  neg_homodimers
 type=heteromers
 msas=$disk/$type
-pdbid=5oxz
-msas_specific=$msas/$pdbid/msas
+#pdbid=5oxz
+#msas_specific=$msas/$pdbid/msas
 FASTA_PATH=$documents/D-SCRIPT/data/seqres/$type
 alpha_dir=$evoformer/alphafold
 PARAMS=$alpha_dir/afdb
 output=$disk/pkl-files
+pdbids_file=$disk/heteromers/ids_heteromers.txt
+lines_to_read=30
 
-singularity exec --nv --bind $home:$home $singularity_img \
+# Read the first 30 lines of the file and perform an action on each line
+while IFS= read -r pdbid && [ $lines_to_read -gt 0 ]; do
+    singularity exec --nv --bind $home:$home $singularity_img \
     python3 $alpha_dir/run_alphafold.py \
         --fasta_paths=$FASTA_PATH/$pdbid.fasta \
         --model_preset=multimer \
-        --msa_dir=$msas_specific \
+        --msa_dir=$msas/$pdbid/msas \
         --output_dir=$output \
         --data_dir=$PARAMS \
         --use_gpu_relax=False \
+        --num_multimer_predictions_per_model=1 \
+        --separate_output_dir=False \
+        --model_to_run="random" \
+done < "$pdbids_file"
+
 

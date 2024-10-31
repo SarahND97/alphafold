e1e554c01c3c1f7cc69d2ffbb9015e9b09738f1e
sarahnd97 <sarahnd97@gmail.com>
2024-10-31 15:33:06 +0100

    remove some print statements and unused code

--- alphafold/data/pipeline.py
+++ alphafold/data/pipeline.py
@@ -329,10 +329,6 @@ class ModifiedDataPipeline:
                 features=template_features, errors=[], warnings=[]
             )
 
-        print("######## input_fasta_str: ###########", input_fasta_str)
-        print("########## input_description: ###########", input_description)
-        # sys.exit()
-
         # paired_out_path = os.path.join(msa_output_dir, "bfd_uniref_hits.a3m")
         hhblits_bfd_uniref_result = run_msa_tool(
             msa_runner=None,
@@ -353,74 +349,4 @@ class ModifiedDataPipeline:
         
         return {**sequence_features, **msa_features, **templates_result.features}
     
-
-class FoldDockPipeline:
-  """Runs the alignment tools and assembles the input features."""
-
-  def __init__(self):
-    pass
-  
-  def process_str(
-        self,
-        input_sequence,
-        input_description = None,
-  ) -> FeatureDict:
-        """Assembles features for a single sequence in a FASTA file""" 
-        num_res = len(input_sequence)
-        sequence_features = make_sequence_features(
-          sequence=input_sequence,
-          description=input_description,
-          num_res=num_res,
-          )
-        return sequence_features
-
-  def process(self, input_fasta_path: str, input_msas: list) -> FeatureDict:
-    """Runs alignment tools on the input sequence and creates features."""
     
-    with open(input_fasta_path) as f:
-        input_fasta_str = f.read()
-        input_seqs, input_descs = parsers.parse_fasta(input_fasta_str)
-        if len(input_seqs) > 2:
-            raise ValueError(
-                f"More than two input sequences found in {input_fasta_path}."
-            )
-        input_sequence_1 = input_seqs[0]
-        input_description_1 = input_descs[0]
-        input_sequence_2 = input_seqs[1]
-        input_description_2 = input_descs[1]
-        # input_description = input_descs[0]
-        num_res = len(input_sequence_1+input_sequence_2)
-    
-    parsed_msas = []
-    parsed_delmat = []
-    for custom_msa in input_msas:
-      msa = ''.join([line for line in open(custom_msa)])
-      if custom_msa[-3:] == 'sto':
-        msa_object = parsers.parse_stockholm(msa)
-        parsed_msa = msa_object.sequences
-        parsed_deletion_matrix = msa_object.deletion_matrix
-      elif custom_msa[-3:] == 'a3m':
-        msa_object = parsers.parse_a3m(msa)
-        parsed_msa = msa_object.sequences
-        parsed_deletion_matrix = msa_object.deletion_matrix
-        # non_zero_elements = [item for sublist in parsed_deletion_matrix for item in (sublist if isinstance(sublist, list) else [sublist]) if item != 0]
-        # print(non_zero_elements ) 
-      else: raise TypeError('Unknown format for input MSA, please make sure '
-                            'the MSA files you provide terminates with (and '
-                            'are formatted as) .sto or .a3m')
-      parsed_msas.append(parsed_msa)
-      parsed_delmat.append(parsed_deletion_matrix)
-
-    # sequence_features = make_sequence_features(
-    #     sequence=input_sequence_1+input_sequence_2,
-    #     description=input_description,
-    #     num_res=num_res)
-
-    msa_features = make_msa_features(
-        msas=parsed_msas, deletion_matrices=parsed_delmat)
-
-    for n, msa in enumerate(parsed_msas):
-        logging.info('MSA %d size: %d sequences.', n, len(msa))
-    logging.info('Final (deduplicated) MSA size: %d sequences.',
-                 msa_features['num_alignments'][0])
-    return {**msa_features} #**sequence_features
